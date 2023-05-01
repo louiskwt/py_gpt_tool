f7a93a171df7b0304c52348ac7d30b540925cfe7
Louis Tsang <kawingt2@gmail.com>
2023-05-01 23:55:07 +0800

    structured my code

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+# Set up and Explore GPT-4
+
+def echo(text):
+    print(f"Hello {text}")
+
+def main():
+    echo("GPT-4")
+    return 0
+
+if __name__ == "__main__":
+    main()
